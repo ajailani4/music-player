f3ccb951f667cf6f4ed5b04ed7b51b5134040d71
Ahmad Jailani <ahmadjailani604@gmail.com>
2023-07-02 13:11:52 +0700

    Update MusicItem

--- app/src/main/java/com/ajailani/musicplayer/ui/screen/home/component/MusicItem.kt
+++ app/src/main/java/com/ajailani/musicplayer/ui/screen/home/component/MusicItem.kt
@@ -14,7 +14,10 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
@@ -35,22 +38,31 @@ fun MusicItem(
             AsyncImage(
                 modifier = Modifier
                     .size(45.dp)
+                    .shadow(
+                        elevation = 1.dp,
+                        shape = MaterialTheme.shapes.small
+                    )
                     .clip(MaterialTheme.shapes.small),
                 model = ImageRequest.Builder(LocalContext.current)
                     .data(music.image)
                     .build(),
+                contentScale = ContentScale.FillBounds,
                 contentDescription = "Music cover"
             )
             Spacer(modifier = Modifier.width(15.dp))
             Column {
                 Text(
                     text = music.title,
-                    style = MaterialTheme.typography.titleMedium
+                    style = MaterialTheme.typography.titleMedium,
+                    maxLines = 1,
+                    overflow = TextOverflow.Ellipsis
                 )
                 Spacer(modifier = Modifier.height(5.dp))
                 Text(
                     text = music.artist,
-                    style = MaterialTheme.typography.bodyMedium
+                    style = MaterialTheme.typography.bodyMedium,
+                    maxLines = 1,
+                    overflow = TextOverflow.Ellipsis
                 )
             }
         }
