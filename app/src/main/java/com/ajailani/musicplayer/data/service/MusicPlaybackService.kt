8137be3fe4d747aa4e010f7344a7d2481f2caf88
Ahmad Jailani <ahmadjailani604@gmail.com>
2023-07-02 18:40:36 +0700

    Update MusicPlaybackService

--- app/src/main/java/com/ajailani/musicplayer/data/service/MusicPlaybackService.kt
+++ app/src/main/java/com/ajailani/musicplayer/data/service/MusicPlaybackService.kt
@@ -1,10 +1,60 @@
 package com.ajailani.musicplayer.data.service
 
+import androidx.media3.common.AudioAttributes
+import androidx.media3.common.C
+import androidx.media3.common.MediaItem
+import androidx.media3.exoplayer.ExoPlayer
 import androidx.media3.session.MediaSession
 import androidx.media3.session.MediaSessionService
+import com.google.common.util.concurrent.Futures
+import com.google.common.util.concurrent.ListenableFuture
 
 class MusicPlaybackService : MediaSessionService() {
-    override fun onGetSession(controllerInfo: MediaSession.ControllerInfo): MediaSession? {
-        TODO("Not yet implemented")
+
+    private lateinit var mediaSession: MediaSession
+    private lateinit var exoPlayer: ExoPlayer
+
+    private val audioAttributes = AudioAttributes.Builder()
+        .setContentType(C.AUDIO_CONTENT_TYPE_MUSIC)
+        .setUsage(C.USAGE_MEDIA)
+        .build()
+
+    override fun onCreate() {
+        super.onCreate()
+
+        initExoPlayer()
+        mediaSession = MediaSession.Builder(this, exoPlayer)
+            .setCallback(MediaSessionCallback())
+            .build()
+    }
+
+    override fun onGetSession(controllerInfo: MediaSession.ControllerInfo) = mediaSession
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        exoPlayer.release()
+        mediaSession.release()
+    }
+
+    private fun initExoPlayer() {
+        exoPlayer = ExoPlayer.Builder(this)
+            .setAudioAttributes(audioAttributes, true)
+            .setHandleAudioBecomingNoisy(true)
+            .build()
+    }
+
+    private inner class MediaSessionCallback : MediaSession.Callback {
+        override fun onAddMediaItems(
+            mediaSession: MediaSession,
+            controller: MediaSession.ControllerInfo,
+            mediaItems: MutableList<MediaItem>
+        ): ListenableFuture<MutableList<MediaItem>> {
+            val updatedMediaItems = mediaItems.map {
+                it.buildUpon().setUri(it.mediaId).build()
+            }.toMutableList()
+
+            return Futures.immediateFuture(updatedMediaItems)
+        }
     }
 }
