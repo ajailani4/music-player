004e696f6f4882aef7481a398acfcb91ad04391e
Ahmad Jailani <ahmadjailani604@gmail.com>
2023-07-03 12:07:29 +0700

    Remove MusicMiniPlaybackCard in MainActivity

--- app/src/main/java/com/ajailani/musicplayer/MainActivity.kt
+++ app/src/main/java/com/ajailani/musicplayer/MainActivity.kt
@@ -6,16 +6,11 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 import androidx.navigation.compose.rememberNavController
 import com.ajailani.musicplayer.data.service.MusicPlaybackService
-import com.ajailani.musicplayer.domain.model.Music
-import com.ajailani.musicplayer.ui.component.MusicMiniPlaybackCard
 import com.ajailani.musicplayer.ui.navigation.Navigation
 import com.ajailani.musicplayer.ui.theme.MusicPlayerTheme
 
@@ -29,24 +24,9 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Box {
-                        val navController = rememberNavController()
+                    val navController = rememberNavController()
 
-                        Navigation(navController)
-
-                        MusicMiniPlaybackCard(
-                            modifier = Modifier
-                                .padding(10.dp)
-                                .align(Alignment.BottomCenter),
-                            music = Music(
-                                id = "1",
-                                title = "Test",
-                                artist = "Someone",
-                                source = "",
-                                image = "https://storage.googleapis.com/uamp/The_Kyoto_Connection_-_Wake_Up/art.jpg"
-                            )
-                        )
-                    }
+                    Navigation(navController)
                 }
             }
         }
